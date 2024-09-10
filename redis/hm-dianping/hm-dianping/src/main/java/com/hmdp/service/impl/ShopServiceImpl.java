304c94e13842afd69a4d074e0f32c856781a001f
kim <2803514404@qq.com>
2024-09-10 20:06:09 +0800

    缓存穿透：设置null值

--- redis/hm-dianping/hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ redis/hm-dianping/hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -15,8 +15,7 @@ import javax.annotation.Resource;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -43,10 +42,17 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
         }
+        //缓存穿透：判断命中的是否为空值
+        if(shopJson!=null){
+            //返回一个错误信息
+            return Result.fail("店铺信息不存在！");
+        }
         //4.不存在，根据id查询数据库
         Shop shop = getById(id);
-        //5.不存在，返回错误 也可以直接设置为null，这样不管存不存在都会加入到缓存
+        //5.不存在，返回错误 就是穿透：也可以直接设置为null，这样不管存不存在都会加入到缓存
         if(shop == null){
+            // 缓存穿透 将空值写入redis
+            stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL,TimeUnit.MINUTES);
             return Result.fail("店铺不存在！");
         }
         //6.存在，写入redis
